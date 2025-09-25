e1083d1788ef1f9b543d214d06140117842796b6
Mio <gusw.0246.lpsyg@gmail.com>
2025-09-25 11:08:56 +0200

    start programming

---
+++ TicTacToe.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        Scanner putX = new Scanner(System.in);
+
+        System.out.println("Binevenido!");
+        table
+        System.out.println("te toca: X");
+        System.out.print("escribe fila y columna: ");
+
+        
+
+
+
+
+
+
+        putX.close();
+
+    }
+    /*
+     * function name : table
+     * char[][]
+     */
+
+     /*
+      * function name : compPlayerMovement
+      *(math.random)
+      */
+
+      /*
+       * function name : winPattern
+       */
+
+}
